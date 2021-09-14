056ef2424086c8b6959bfbfc67642c4bd6dd657e
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2021-09-14 11:24:19 +0500

    languages add

--- myresume/urls.py
+++ myresume/urls.py
@@ -13,9 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('118n', include('django.conf.urls.i18n'))
 ]
+
+urlpatterns += i18n_patterns(
+    # path('', include('app.urls')),
+)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
