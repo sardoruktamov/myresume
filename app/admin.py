06c668f926a717b8e0ace27b807b1484eddf4a47
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2021-09-15 21:21:00 +0500

    admin panel full create

--- app/admin.py
+++ app/admin.py
@@ -9,12 +9,28 @@ from .models import (
 
 admin.site.register(Contact_me)
 admin.site.register(Skills)
+
+
 # admin.site.register(Portfolio)
 # Register your models here.
 @admin.register(About)
 class AboutAdmin(TranslatableAdmin):
-    list_display = ('title', 'description','address','email','phone', 'language',)
+    list_display = ('title', 'description', 'address', 'email', 'phone', 'language',)
+
 
 @admin.register(Portfolio)
 class PortfolioAdmin(TranslatableAdmin):
     list_display = ('title',)
+
+
+@admin.register(Experience)
+class ExperienceAdmin(TranslatableAdmin):
+    list_display = ('description', 'techno', 'from_date', 'to_date', 'dagree',)
+
+@admin.register(Education)
+class EducationAdmin(TranslatableAdmin):
+    list_display = ('name', 'description', 'from_date', 'to_date', 'dagree',)
+
+@admin.register(References)
+class ReferencesAdmin(TranslatableAdmin):
+    list_display = ('full_name', 'description', 'techno', 'image',)
