d994d73125dfb59794e9b137c67f4db9e4dcfe6f
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2021-09-17 13:35:10 +0500

    handler404 create

--- app/views.py
+++ app/views.py
@@ -77,22 +77,7 @@ def sendmail(request):
         )
         contact_user.save()
         return render(request, 'sendmail.html')
-    else:
-        contactme = Contact_me.objects.all()
-        about = About.objects.all()
-        skills = Skills.objects.all()
-        portfolio = Portfolio.objects.all()
-        experience = Experience.objects.all()
-        education = Education.objects.all()
-        reference = References.objects.all()
 
-        context = {
-            'contactme': contactme,
-            'about': about,
-            'skills': skills,
-            'portfolio': portfolio,
-            'experience': experience,
-            'education': education,
-            'reference': reference
-        }
-        return render(request, 'index.html', context)
+
+def error_404_view(request, exception):
+    return render(request, "404.html")
--- myresume/settings.py
+++ myresume/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-90*sr)_9x)luys3y!e^j1k$d%x3*n#t%=4imjbltc&np)5&rcq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
