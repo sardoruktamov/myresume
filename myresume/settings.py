fcc32c79acaaae74ec780e058a6c5b73dd502e90
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2021-12-09 21:05:39 +0500

    debug false

--- myresume/settings.py
+++ myresume/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-90*sr)_9x)luys3y!e^j1k$d%x3*n#t%=4imjbltc&np)5&rcq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
